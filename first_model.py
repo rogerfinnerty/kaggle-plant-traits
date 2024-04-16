1c15f9288ae466e6d11cfba87cc9b3e8519c325e
Roger Finnerty <jrfinn@bu.edu>
2024-04-16 17:55:07 -0400

    Starter code

---
+++ first_model.py
@@ -0,0 +1,366 @@
+"""
+https://www.kaggle.com/competitions/planttraits2024
+"""
+
+import os
+# os.environ["KERAS_BACKEND"] = "jax" # you can also use tensorflow or torch
+import math
+import numpy as np
+# import keras
+from tensorflow import keras
+from tensorflow.keras import ops
+import keras_cv
+import pandas as pd
+import tensorflow as tf
+from sklearn.model_selection import StratifiedKFold
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+
+class CFG:
+    """
+    Configuration
+    """
+    verbose = 1  # verbosity
+    seed = 42  # Random seed
+    preset = "efficientnetv2_b2_imagenet"  # Name of pretrained classifier
+    image_size = [224, 224]  # Input image size
+    epochs = 12 # Training epochs
+    batch_size = 96  # Batch size
+    lr_mode = "step" # LR scheduler mode from one of "cos", "step", "exp"
+    drop_remainder = True  # Drop incomplete batches
+    num_classes = 6 # Number of classes in the dataset
+    num_folds = 5 # Number of folds to split the dataset
+    fold = 0 # Which fold to set as validation data
+    class_names = ['X4_mean', 'X11_mean', 'X18_mean',
+                   'X26_mean', 'X50_mean', 'X3112_mean',]
+    aux_class_names = list(map(lambda x: x.replace("mean","sd"), class_names))
+    num_classes = len(class_names)
+    aux_num_classes = len(aux_class_names)
+
+keras.utils.set_random_seed(CFG.seed)
+BASE_PATH = "planttraits2024"
+
+### Train + validation ###
+df = pd.read_csv(f'{BASE_PATH}/train.csv')
+df['image_path'] = f'{BASE_PATH}/train_images/'+df['id'].astype(str)+'.jpeg'
+# fill N/A with -1
+df.loc[:, CFG.aux_class_names] = df.loc[:, CFG.aux_class_names].fillna(-1)
+# print(df.head(2))
+
+### Test ###
+test_df = pd.read_csv(f'{BASE_PATH}/test.csv')
+test_df['image_path'] = f'{BASE_PATH}/test_images/'+test_df['id'].astype(str)+'.jpeg'
+FEATURE_COLS = test_df.columns[1:-1].tolist()
+# print(test_df.head(2))
+
+def build_augmenter():
+    """Data augmentations"""
+    # define augmentations
+    aug_layers = [
+        keras_cv.layers.RandomBrightness(factor=0.1, value_range=(0,1)),
+        keras_cv.layers.RandomContrast(factor=0.1, value_range=(0,1)),
+        keras_cv.layers.RandomSaturation(factor=(0.45,0.55)),
+        keras_cv.layers.RandomHue(factor=0.1, value_range=(0,1)),
+        keras_cv.layers.RandomCutout(height_factor=(0.06, 0.15), width_factor=(0.06, 0.15)),
+        keras_cv.layers.RandomFlip(mode='horizontal_and_vertical'),
+        keras_cv.layers.RandomZoom(height_factor=(0.05, 0.15)),
+        keras_cv.layers.RandomRotation(factor=(0.01, 0.05))
+    ]
+    # apply augmentations to random samples
+    aug_layers = [keras_cv.layers.RandomApply(x, rate=0.5) for x in aug_layers]
+
+    # build augmentation layer
+    augmenter = keras_cv.layers.Augmenter(aug_layers)
+
+    # apply augmentations
+    def augment(inp, label):
+        images = inp['images']
+        aug_data = {'images': images}
+        aug_data = augmenter(aug_data)
+        inp['images'] = aug_data['images']
+        return inp, label
+
+    return augment
+
+def build_decoder(with_labels=True, target_size=CFG.image_size):
+    def decode_image(inp):
+        path = inp['images']
+
+        # read jpeg images
+        file_bytes = tf.io.read_file(path)
+        image = tf.io.decode_jpeg(file_bytes)
+        # resize
+        image = tf.image.resize(image, size=target_size, method='area')
+        # rescale
+        image = tf.cast(image, tf.float32)
+        image /= 255.0
+        # reshape
+        image = tf.reshape(image, [*target_size, 3])
+        inp['images'] = image
+        return inp
+
+    def decode_label(label, num_classes):
+        label = tf.cast(label, tf.float32)
+        label = tf.reshape(label, [num_classes])
+        return label
+
+    def decode_with_labels(inp, labels=None):
+        inp = decode_image(inp)
+        label = decode_label(labels[0], CFG.num_classes)
+        aux_label = decode_label(labels[1], CFG.aux_num_classes)
+        return (inp, (label, aux_label))
+
+    return decode_with_labels if with_labels else decode_image
+
+def build_dataset(
+    paths,
+    features,
+    labels=None,
+    aux_labels=None,
+    batch_size=32,
+    cache=True,
+    decode_fn=None,
+    augment_fn=None,
+    augment=False,
+    repeat=True,
+    shuffle=1024,
+    cache_dir="",
+    drop_remainder=False,
+):
+    if cache_dir != "" and cache is True:
+        os.makedirs(cache_dir, exist_ok=True)
+
+    if decode_fn is None:
+        decode_fn = build_decoder(labels is not None or aux_labels is not None)
+
+    if augment_fn is None:
+        augment_fn = build_augmenter()
+
+    AUTO = tf.data.experimental.AUTOTUNE
+
+    inp = {"images": paths, "features": features}
+    slices = (inp, (labels, aux_labels)) if labels is not None else inp
+
+    ds = tf.data.Dataset.from_tensor_slices(slices)
+    ds = ds.map(decode_fn, num_parallel_calls=AUTO)
+    ds = ds.cache(cache_dir) if cache else ds
+    ds = ds.repeat() if repeat else ds
+    if shuffle:
+        ds = ds.shuffle(shuffle, seed=CFG.seed)
+        opt = tf.data.Options()
+        opt.experimental_deterministic = False
+        ds = ds.with_options(opt)
+    ds = ds.batch(batch_size, drop_remainder=drop_remainder)
+    ds = ds.map(augment_fn, num_parallel_calls=AUTO) if augment else ds
+    ds = ds.prefetch(AUTO)
+    return ds
+
+
+# split the data into 5 stratified folds; first create bins based on 6 plant traits
+# distributions and combine them into a final bin column
+# then, use this bin for balancing similar traits distributions across all folds.
+skf = StratifiedKFold(n_splits=CFG.num_folds, shuffle=True, random_state=42)
+
+# Create separate bin for each traits
+for i, trait in enumerate(CFG.class_names):
+
+    # Determine the bin edges dynamically based on the distribution of traits
+    bin_edges = np.percentile(df[trait], np.linspace(0, 100, CFG.num_folds + 1))
+    df[f"bin_{i}"] = np.digitize(df[trait], bin_edges)
+
+# Concatenate the bins into a final bin
+df["final_bin"] = (
+    df[[f"bin_{i}" for i in range(len(CFG.class_names))]]
+    .astype(str)
+    .agg("".join, axis=1)
+)
+
+# Perform the stratified split using final bin
+df = df.reset_index(drop=True)
+for fold, (train_idx, valid_idx) in enumerate(skf.split(df, df["final_bin"])):
+    df.loc[valid_idx, "fold"] = fold
+
+# Create train and validation data loaders
+# Tabular features (ancillary data) consist only of continuous,
+# non categorical features (don't need to do any encoding)
+# Before passing features to the model, we apply standard normalization,
+# ensuring that features have consistent scales
+
+# Sample from full data
+sample_df = df.copy()
+train_df = sample_df[sample_df.fold != CFG.fold]
+valid_df = sample_df[sample_df.fold == CFG.fold]
+print(f"# Num Train: {len(train_df)} | Num Valid: {len(valid_df)}")
+
+# Normalize features
+scaler = StandardScaler()
+train_features = scaler.fit_transform(train_df[FEATURE_COLS].values)
+valid_features = scaler.transform(valid_df[FEATURE_COLS].values)
+
+# Train
+train_paths = train_df.image_path.values
+train_labels = train_df[CFG.class_names].values
+train_aux_labels = train_df[CFG.aux_class_names].values
+train_ds = build_dataset(train_paths, train_features, train_labels, train_aux_labels,
+                         batch_size=CFG.batch_size,
+                         repeat=True, shuffle=True, augment=True, cache=False)
+
+# Valid
+valid_paths = valid_df.image_path.values # len B_v
+valid_labels = valid_df[CFG.class_names].values # B_v x 6
+valid_aux_labels = valid_df[CFG.aux_class_names].values # B_v x 6
+valid_ds = build_dataset(valid_paths, valid_features, valid_labels, valid_aux_labels,
+                         batch_size=CFG.batch_size,
+                         repeat=False, shuffle=False, augment=False, cache=False)
+
+inps, tars = next(iter(train_ds))
+imgs = inps["images"]
+num_imgs, num_cols = 8, 4
+
+plt.figure(figsize=(4 * num_cols, num_imgs // num_cols * 5))
+for i, (img, tar) in enumerate(zip(imgs[:num_imgs], tars[0][:num_imgs])):
+    plt.subplot(num_imgs // num_cols, num_cols, i + 1)
+    img = img. numpy()
+    tar = tar.numpy()
+    
+    img = (img - img.min()) / (img.max() + 1e-4)
+
+    formatted_tar = "\n".join(
+        [
+            ", ".join(
+                f"{name.replace('_mean','')}: {val:.2f}"
+                for name, val in zip(CFG.class_names[j : j + 3], tar[j : j + 3])
+            )
+            for j in range(0, len(CFG.class_names), 3)
+        ]
+    )
+
+    plt.imshow(img)
+    plt.title(f"[{formatted_tar}]")
+    plt.axis("off")
+
+plt.tight_layout()
+plt.show()
+
+class R2Loss(keras.losses.Loss):
+    def __init__(self, use_mask=False, name="r2_loss"):
+        super().__init__(name=name)
+        self.use_mask = use_mask
+
+    def call(self, y_true, y_pred):
+        if self.use_mask:
+            mask = (y_true != -1)
+            y_true = ops.where(mask, y_true, 0.0)
+            y_pred = ops.where(mask, y_pred, 0.0)
+        SS_res = ops.sum(ops.square(y_true - y_pred), axis=0)  # (B, C) -> (C,)
+        SS_tot = ops.sum(ops.square(y_true - ops.mean(y_true, axis=0)), axis=0)  # (B, C) -> (C,)
+        r2_loss = SS_res / (SS_tot + 1e-6)  # (C,)
+        return ops.mean(r2_loss)  # ()
+    
+class R2Metric(keras.metrics.Metric):
+    def __init__(self, name="r2", **kwargs):
+        super(R2Metric, self).__init__(name=name, **kwargs)
+        self.SS_res = self.add_weight(name='SS_res', shape=(6,), initializer='zeros')
+        self.SS_tot = self.add_weight(name='SS_tot', shape=(6,) ,initializer='zeros')
+        self.num_samples = self.add_weight(name='num_samples', initializer='zeros')
+
+    def update_state(self, y_true, y_pred, sample_weight=None):
+        SS_res = ops.sum(ops.square(y_true - y_pred), axis=0)
+        SS_tot = ops.sum(ops.square(y_true - ops.mean(y_true, axis=0)), axis=0)
+        self.SS_res.assign_add(SS_res)
+        self.SS_tot.assign_add(SS_tot)
+        self.num_samples.assign_add(ops.cast(ops.shape(y_true)[0], "float32"))
+
+    def result(self):
+        r2 = 1 - self.SS_res / (self.SS_tot + 1e-6)
+        return ops.mean(r2)
+
+    def reset_states(self):
+        self.total_SS_res.assign(0)
+        self.total_SS_tot.assign(0)
+        self.num_samples.assign(0)
+
+# Define input layers
+img_input = keras.Input(shape=(*CFG.image_size, 3), name="images")
+feat_input = keras.Input(shape=(len(FEATURE_COLS),), name="features")
+
+# Branch for image input
+backbone = keras_cv.models.EfficientNetV2Backbone.from_preset(CFG.preset)
+x1 = backbone(img_input)
+x1 = keras.layers.GlobalAveragePooling2D()(x1)
+x1 = keras.layers.Dropout(0.2)(x1)
+
+# Branch for tabular/feature input
+x2 = keras.layers.Dense(326, activation="selu")(feat_input)
+x2 = keras.layers.Dense(64, activation="selu")(x2)
+x2 = keras.layers.Dropout(0.1)(x2)
+
+# Concatenate both branches
+concat = keras.layers.Concatenate()([x1, x2])
+
+# Output layer
+out1 = keras.layers.Dense(CFG.num_classes, activation=None, name="head")(concat)
+out2 = keras.layers.Dense(CFG.aux_num_classes, activation="relu", name="aux_head")(concat)
+out = {"head": out1, "aux_head":out2}
+
+# Build model
+model = keras.models.Model([img_input, feat_input], out)
+
+# Compile the model
+model.compile(
+    optimizer=keras.optimizers.Adam(learning_rate=1e-4),
+    loss={
+        "head": R2Loss(use_mask=False),
+        "aux_head": R2Loss(use_mask=True), # use_mask to ignore `NaN` auxiliary labels
+    },
+    loss_weights={"head": 1.0, "aux_head": 0.3},  # more weight to main task
+    metrics={"head": R2Metric()}, # evaluation metric only on main task
+)
+
+# Model Summary
+model.summary()
+
+def get_lr_callback(batch_size=8, mode='cos', epochs=10, plot=False):
+    '''
+    Create learning rate scheduler
+    '''
+    lr_start, lr_max, lr_min = 5e-5, 8e-6 * batch_size, 1e-5
+    lr_ramp_ep, lr_sus_ep, lr_decay = 3, 0, 0.75
+
+    def lrfn(epoch):  # Learning rate update function
+        if epoch < lr_ramp_ep: lr = (lr_max - lr_start) / lr_ramp_ep * epoch + lr_start
+        elif epoch < lr_ramp_ep + lr_sus_ep: lr = lr_max
+        elif mode == 'exp': lr = (lr_max - lr_min) * lr_decay**(epoch - lr_ramp_ep - lr_sus_ep) + lr_min
+        elif mode == 'step': lr = lr_max * lr_decay**((epoch - lr_ramp_ep - lr_sus_ep) // 2)
+        elif mode == 'cos':
+            decay_total_epochs, decay_epoch_index = epochs - lr_ramp_ep - lr_sus_ep + 3, epoch - lr_ramp_ep - lr_sus_ep
+            phase = math.pi * decay_epoch_index / decay_total_epochs
+            lr = (lr_max - lr_min) * 0.5 * (1 + math.cos(phase)) + lr_min
+        return lr
+
+    if plot:  # Plot lr curve if plot is True
+        plt.figure(figsize=(10, 5))
+        plt.plot(np.arange(epochs), [lrfn(epoch) for epoch in np.arange(epochs)], marker='o')
+        plt.xlabel('epoch'); plt.ylabel('lr')
+        plt.title('LR Scheduler')
+        plt.show()
+
+    return keras.callbacks.LearningRateScheduler(lrfn, verbose=False)  # Create lr callback
+
+lr_cb = get_lr_callback(CFG.batch_size, mode=CFG.lr_mode, plot=True)
+ckpt_cb = keras.callbacks.ModelCheckpoint(
+    "best_model.keras",
+    monitor="val_head_r2",
+    save_best_only=True,
+    save_weights_only=False,
+    mode="max",
+)
+
+history = model.fit(
+    train_ds,
+    epochs=CFG.epochs,
+    callbacks=[lr_cb, ckpt_cb],
+    steps_per_epoch=len(train_df) // CFG.batch_size,
+    validation_data=valid_ds,
+    verbose=CFG.verbose,
+)
