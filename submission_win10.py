ad116ab1cc346074a08169f108a43ace00fd8a82
benburnham <burnham1@bu.edu>
2024-04-25 20:57:52 -0400

    cleaned and added submission files

--- submission_win10.py
+++ submission_win10.py
@@ -39,14 +39,6 @@ class CFG:
     num_classes = len(class_names)
     aux_num_classes = len(aux_class_names)
 
-BASE_PATH = '../planttraits2024/'
-
-# Test
-test_df = pd.read_csv(f'{BASE_PATH}/test.csv')
-test_df['image_path'] = f'{BASE_PATH}/test_images/'+test_df['id'].astype(str)+'.jpeg'
-FEATURE_COLS = test_df.columns[1:-1].tolist()
-print(test_df.head(2))
-
 class PlantDataset(Dataset):
     def __init__(self, paths, features, labels=None, aux_labels=None, transform=None, augment=False):
         self.paths = paths
@@ -113,13 +105,62 @@ def build_dataset(paths, features, labels=None, aux_labels=None, batch_size=32, 
 
     return dataloader
 
-# Assuming df is your dataframe containing file paths, features, labels, and fold information
-skf = StratifiedKFold(n_splits=CFG.num_folds, shuffle=True, random_state=42)
-
-
+# Original base
 class CustomModel(nn.Module):
-    def __init__(self, num_classes, aux_num_classes, feature_cols, model_name):
+    def __init__(self, num_classes, aux_num_classes, feature_cols, model_name, lin_lay = 1600):
         super(CustomModel, self).__init__()
+
+        # Define input layers
+        self.img_input = nn.Identity()
+        self.feat_input = nn.Identity()
+
+        # Load pre-trained model
+        self.backbone = timm.create_model(model_name, 
+                                          pretrained=True, 
+                                          global_pool='avg'
+                                          )
+        
+        # Adapt the model to match the expected output size
+        # self.backbone.global_pool = nn.AdaptiveAvgPool2d(1)
+        self.backbone.classifier = nn.Identity()
+
+        self.dropout_img = nn.Dropout(0.2)
+
+        # Branch for tabular/feature input
+        self.dense1 = nn.Linear(len(feature_cols), 326)
+        self.dense2 = nn.Linear(326, 64)
+        self.dropout_feat = nn.Dropout(0.1)
+
+        # Output layer
+        self.head = nn.Linear(lin_lay, num_classes)
+        self.aux_head = nn.Linear(lin_lay, aux_num_classes)
+
+        print(f'Model Name: {model_name}')
+        print(f'Model pooling: {self.backbone.global_pool}')
+        print(f'Model classifier: {self.backbone.get_classifier()}')
+
+    def forward(self, img, feat):
+        # Image branch
+        x1 = self.backbone(img)
+        x1 = self.dropout_img(x1.flatten(1))
+
+        # Feature branch
+        x2 = F.selu(self.dense1(self.feat_input(feat)))
+        x2 = F.selu(self.dense2(x2))
+        x2 = self.dropout_feat(x2)
+
+        # Concatenate both branches
+        concat = torch.cat([x1, x2], dim=1)
+        # Output layer
+        out1 = self.head(concat)
+        out2 = F.relu(self.aux_head(concat))
+
+        return {'head': out1, 'aux_head': out2}
+  
+# resnet_v2
+class CustomModel2(nn.Module):
+    def __init__(self, num_classes, aux_num_classes, feature_cols, model_name):
+        super(CustomModel2, self).__init__()
 
         # Define input layers
         self.img_input = nn.Identity()
@@ -181,49 +222,83 @@ class CustomModel(nn.Module):
 
         return {'head': out1, 'aux_head': out2}
 
+  
+if __name__ == '__main__':
 
-# Instantiate the model with the desired EfficientNetV2 model_name
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = CustomModel(CFG.num_classes, CFG.aux_num_classes, FEATURE_COLS, model_name=CFG.model_name)
-model.to(device)
-model.load_state_dict(torch.load('../resnet50.a1_in1k_best_model2_2.pth', map_location=device))
+    BASE_PATH = 'planttraits2024/'
 
-# Test
-test_paths = test_df.image_path.values
-scaler = StandardScaler()
-test_features = scaler.fit_transform(test_df[FEATURE_COLS].values) 
-test_ds = build_dataset(test_paths, test_features, batch_size=CFG.batch_size,
-                         repeat=False, shuffle=False, augment=False, cache=False)
+    # Test
+    test_df = pd.read_csv(f'{BASE_PATH}/test.csv')
+    test_df['image_path'] = f'{BASE_PATH}/test_images/'+test_df['id'].astype(str)+'.jpeg'
+    FEATURE_COLS = test_df.columns[1:-1].tolist()
+    print(test_df.head(2))  
 
-model.eval()  # Set the model to evaluation mode
+    # Assuming df is your dataframe containing file paths, features, labels, and fold information
+    skf = StratifiedKFold(n_splits=CFG.num_folds, shuffle=True, random_state=42)
 
-# List to store predictions
-all_predictions = []
+    # Instantiate the model with the desired EfficientNetV2 model_name
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-# Iterate over batches in the test data loader
-for batch_idx, inputs_dict in enumerate(tqdm(test_ds, desc='Testing')):
-    # Extract images and features from the inputs_dict
-    inputs_images = inputs_dict['images'].to(device, dtype=torch.float32)  # Assuming 'device' is the target device
-    inputs_features = inputs_dict['features'].to(device, dtype=torch.float32)
+    # resnet50.a1_in1k IMPROVED
+    model = CustomModel2(CFG.num_classes, CFG.aux_num_classes, FEATURE_COLS, model_name='resnet50.a1_in1k')
+    out_name = "resnet_V2.csv"
+    model_weights = 'resnet50.a1_in1k_best_model2_2.pth'
 
-    # Forward pass
-    with torch.no_grad():
-        outputs = model(inputs_images, inputs_features)
+    # resnet50.a1_in1k
+    # model = CustomModel(CFG.num_classes, CFG.aux_num_classes, FEATURE_COLS, model_name='resnet50.a1_in1k')
+    # out_name = "resnet.csv"
+    # model_weights = 'resnet50.a1_in1k_best_model2V2.pth'
 
-    # Get predictions
-    predictions = outputs['head'].cpu().numpy()  # Assuming 'head' is the main task output
+    # efficientnet_b3.ra2_in1k
+    # model = CustomModel(CFG.num_classes, CFG.aux_num_classes, FEATURE_COLS, model_name='efficientnet_b3.ra2_in1k')
+    # out_name = "efficientnet.csv"
+    # model_weights = 'efficientnet_b3.ra2_in1k_best_model2.pth'
 
-    # Append predictions to the list
-    all_predictions.append(predictions)
+    # maxvit_tiny_tf_384.in1k
+    # model = CustomModel(CFG.num_classes, CFG.aux_num_classes, FEATURE_COLS, model_name='maxvit_tiny_tf_384.in1k', lin_lay=1064)
+    # out_name = "maxvit.csv"
+    # model_weights = 'maxvit_tiny_tf_384.in1k_best_model.pth'
+    
+    model.to(device)
+    model.load_state_dict(torch.load(model_weights, map_location=device))
 
-# Concatenate predictions for all batches
-all_predictions = np.concatenate(all_predictions, axis=0)
+    # Test
+    test_paths = test_df.image_path.values
+    scaler = StandardScaler()
+    test_features = scaler.fit_transform(test_df[FEATURE_COLS].values) 
+    test_ds = build_dataset(test_paths, test_features, batch_size=CFG.batch_size,
+                            repeat=False, shuffle=False, augment=False, cache=False)
 
-pred_df = test_df[["id"]].copy()
-target_cols = [x.replace("_mean","") for x in CFG.class_names]
-pred_df[target_cols] = all_predictions.tolist()
+    model.eval()  # Set the model to evaluation mode
 
-sub_df = pd.read_csv('template_sub.csv')
-sub_df = sub_df[["id"]].copy()
-sub_df = sub_df.merge(pred_df, on="id", how="left")
-sub_df.to_csv("sub_out_job.csv", index=False)
+    # List to store predictions
+    all_predictions = []
+
+    # Iterate over batches in the test data loader
+    for batch_idx, inputs_dict in enumerate(tqdm(test_ds, desc='Testing')):
+        # Extract images and features from the inputs_dict
+        inputs_images = inputs_dict['images'].to(device, dtype=torch.float32)  # Assuming 'device' is the target device
+        inputs_features = inputs_dict['features'].to(device, dtype=torch.float32)
+
+        # Forward pass
+        with torch.no_grad():
+            outputs = model(inputs_images, inputs_features)
+
+        # Get predictions
+        predictions = outputs['head'].cpu().numpy()  # Assuming 'head' is the main task output
+
+        # Append predictions to the list
+        all_predictions.append(predictions)
+
+    # Concatenate predictions for all batches
+    all_predictions = np.concatenate(all_predictions, axis=0)
+
+    pred_df = test_df[["id"]].copy()
+    target_cols = [x.replace("_mean","") for x in CFG.class_names]
+    pred_df[target_cols] = all_predictions.tolist()
+
+    sub_df = pd.read_csv(f'sample_sub.csv')
+    sub_df = sub_df[["id"]].copy()
+    sub_df = sub_df.merge(pred_df, on="id", how="left")
+    sub_df.to_csv(out_name, index=False)
+    sub_df
